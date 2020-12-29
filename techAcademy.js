ee1bd4b7d2ca9b031216c4d32fbe56a04798cca8
大平　明日河 <rozeo1505@gmail.com>
2020-12-29 23:18:52 +0900

    Signed-off-by: 大平　明日河 <rozeo1505@gmail.com> なぜか間違っていたので修正

--- techAcademy.js
+++ techAcademy.js
@@ -1,11 +1,10 @@
-//ボタンをオスと、ブロック内のプログラムを実行する。
-$("btn1").on("click", function() {
-let date = new Date();
-let year = data.getFullYear();
-let month = date.getMonth() + 1;
-let day = date.getDate();
-let message = '今日は${year}年${month}月${day}日です。'
-// jQueryを使って画面にメッセージを表示する。
-$("#tBox").val(message);
-}
-);
+// ボタンを押すと、ブロック内のプログラムが実行される
+$("#btn1").on("click", function()  {
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  let message = `今日は${year}年${month}月${day}日です`
+  // jQueryを使って画面にメッセージを表示する
+  $("#tBox").val(message);
+});
